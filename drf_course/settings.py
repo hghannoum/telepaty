5cad875d8d7d79f78e87638092ccdc941781965a
Halima <81455531+hghannoum@users.noreply.github.com>
2023-09-26 10:45:31 +0300

    Update settings.py

--- drf_course/settings.py
+++ drf_course/settings.py
@@ -33,13 +33,42 @@ INSTALLED_APPS = [
     'core',
 ]
 
+def parse_redis_url(url):
+    """ parses a redis url into component parts, stripping password from the host.
+    Long keys in the url result in parsing errors, since labels within a hostname cannot exceed 64 characters under
+    idna rules.
+    In that event, we remove the key/password so that it can be passed separately to the RedisChannelLayer.
+    Heroku REDIS_URL does not include the DB number, so we allow for a default value of '0'
+    """
+    parsed = urlparse(url)
+    parts = parsed.netloc.split(':')
+    host = ':'.join(parts[0:-1])
+    port = parts[-1]
+    path = parsed.path.split('/')[1:]
+    db = int(path[0]) if len(path) >= 1 else 0
+
+    user, password = (None, None)
+    if '@' in host:
+        creds, host = host.split('@')
+        user, password = creds.split(':')
+        host = f'{user}@{host}'
+
+    return host, port, user, password, db
+
+REDIS_URL = env('rediss://red-ck845c08elhc73do0tb0:KNbKJkYsQeQUbUaeg1QTqtxZ6CJw410A@oregon-redis.render.com:6379', default='redis://localhost:6379')
+REDIS_HOST, REDIS_PORT, REDIS_USER, REDIS_PASSWORD, REDIS_DB = parse_redis_url(REDIS_URL)
+# DJANGO CHANNELS
 CHANNEL_LAYERS = {
     'default': {
-        'LOCATION': 'rediss://red-ck845c08elhc73do0tb0:KNbKJkYsQeQUbUaeg1QTqtxZ6CJw410A@oregon-redis.render.com:6379/0',  # Replace with your Redis URL
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        }
-    }
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [{
+                'address': f'redis://{REDIS_HOST}:{REDIS_PORT}',
+                'db': REDIS_DB,
+                'password': REDIS_PASSWORD,
+            }],
+        },
+    },
 }
 
 
